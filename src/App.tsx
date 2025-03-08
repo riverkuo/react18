907b5eb7d8dfa41d4e810c5c0758774f2b49e767
River <coco0958231520@gmail.com>
2025-03-08 18:22:13 +0800

    init and add packages

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+import { Home } from './layput/home';
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/layput/home/index.tsx
@@ -0,0 +1,10 @@
+import { Test } from '@/components/base/test';
+
+export function Home() {
+  return (
+    <div>
+      <h1>Hello World! This is River react 18 project!</h1>
+      <Test />
+    </div>
+  );
+}
