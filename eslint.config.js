5f03ecd63b72f34f86ac5ddf33b90e15d98b4912
River <coco0958231520@gmail.com>
2025-03-12 18:10:48 +0800

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,7 @@ export default tseslint.config(
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
       '@typescript-eslint/no-unused-vars': ['warn'],
-      'no-unused-vars': ['warn'],
+      '@typescript-eslint/no-explicit-any': ['warn'],
     },
   }
 );
