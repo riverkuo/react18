051f1fccab3ca133530cde64a8b6d610af64cb88
River <coco0958231520@gmail.com>
2025-04-27 18:30:38 +0800

    remove unused

--- src/layput/home/index.tsx
+++ src/layput/home/index.tsx
@@ -1,10 +1,8 @@
-import { Test } from '@/components/base/test';
 
 export function Home() {
   return (
     <div>
       <h1>Hello World! This is River react 18 project!</h1>
-      <Test />
     </div>
   );
 }
