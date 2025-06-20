943a1358e4f207a245ec586a4598af9d944465a1
River <river@jgallop.com>
2025-06-20 10:19:28 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Home } from './layput/home';
 function App() {
+  console.log('access server base url from secrets', process.env.REACT_APP_SERVER_BASE_URL);
   return <Home />;
 }
 
