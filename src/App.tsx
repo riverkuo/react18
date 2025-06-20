c322b440bce21e869abc0769ee9ecf99bb101551
River <river@jgallop.com>
2025-06-20 10:20:30 +0800

    remove test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { Home } from './layput/home';
 function App() {
-  console.log('access server base url from secrets', process.env.REACT_APP_SERVER_BASE_URL);
   return <Home />;
 }
 
